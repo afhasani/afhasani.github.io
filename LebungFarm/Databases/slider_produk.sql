0872cc4414d57c451928e11e052869974fce9c1e
Ahmad Faqih Hasani <167625310+afhasani@users.noreply.github.com>
2024-11-06 00:40:34 +0700

    Delete LebungFarm directory

--- LebungFarm/Databases/slider_produk.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE sliderDB;
-
-USE sliderDB;
-
-CREATE TABLE sliders (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    slider_group INT NOT NULL, -- 1 untuk slider pertama, 2 untuk slider kedua
-    image_url VARCHAR(255) NOT NULL
-);
